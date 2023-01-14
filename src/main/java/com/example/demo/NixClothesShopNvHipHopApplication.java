418bdacc5886519eacc743299e12fad791edead4
DIN-GS <nikita.ivakin@nure.ua>
2023-01-14 09:47:00 +0200

    first commit

---
+++ src/main/java/com/example/demo/NixClothesShopNvHipHopApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NixClothesShopNvHipHopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NixClothesShopNvHipHopApplication.class, args);
+	}
+
+}
